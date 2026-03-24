8822ce712826477aa1a4bdb44e27b4a2f69263c6
bobby <git@qq.com>
2026-03-24 21:24:07 +0800

    后端增删改查功能完成

--- src/main/java/com/bobby/aicode/AiCodeApplication.java
+++ src/main/java/com/bobby/aicode/AiCodeApplication.java
@@ -1,9 +1,13 @@
 package com.bobby.aicode;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@MapperScan("com.bobby.aicode.mapper")
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class AiCodeApplication {
 
     public static void main(String[] args) {
